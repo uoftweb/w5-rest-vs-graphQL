af04ae75736d77278e87bc682ba49fb3fedc233e
MaXeraph <maxpham99@gmail.com>
2021-04-30 18:19:46 -0400

    Refactor

--- utils.js
+++ utils.js
@@ -1,42 +1,52 @@
-const fs = require('fs');
+const fs = require("fs");
 
-
-function readData(){
-    const rawdata = fs.readFileSync('data.json');
-    return JSON.parse(rawdata);
+function readData() {
+  const rawdata = fs.readFileSync("data.json");
+  return JSON.parse(rawdata);
 }
 
-function writeData(courses){
-    fs.writeFileSync('data.json', JSON.stringify(courses));
+function writeData(courses) {
+  fs.writeFileSync("data.json", JSON.stringify(courses));
 }
 
-function getCourseByID(id){
-    const courses = readData()
-    return courses[id];
+function getCourseByID(id) {
+  const courses = readData();
+  return courses[id];
 }
 
-function getCourses(id){
-    const courses = readData()
-    return courses;
+function getCourses(id) {
+  const courses = readData();
+  return courses;
 }
 
 function createCourse(course) {
-    const courses = readData()
-    const fields = ["courseCode", "title", "hours", "summary"]
+  const courses = readData();
+  const fields = [
+    "courseCode",
+    "title",
+    "hours",
+    "summary",
+    "prerequisites",
+    "exclusions",
+    "recommended",
+    "distribution",
+    "breadth",
+    "program",
+  ];
 
-    for (const field of fields){
-        if (!course.hasOwnProperty(field)){
-            throw new Error("Data ill-defined")
-        }
+  for (const field of fields) {
+    if (!course.hasOwnProperty(field)) {
+      throw new Error(`Course missing field: ${field}`);
     }
+  }
 
-    if (courses[course.courseCode]){
-        throw new Error("Course existed")
-    }
+  if (courses[course.courseCode]) {
+    throw new Error("Course existed");
+  }
 
-    courses[course.courseCode] = course
+  courses[course.courseCode] = course;
 
-    writeData(courses)
+  writeData(courses);
 }
 
-module.exports = {getCourseByID, getCourses, createCourse}
+module.exports = { getCourseByID, getCourses, createCourse };
